f3a7c1af3b090585bb7ff618f23261a12d68ddad
Sayan <msayanece@gmail.com>
2018-09-16 20:50:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sayan/hardwaredisable/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sayan.hardwaredisable;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+
+        activityManager.moveTaskToFront(getTaskId(), 0);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Do nothing or catch the keys you want to block
+        return false;
+    }
+}
